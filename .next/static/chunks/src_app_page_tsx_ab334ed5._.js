d054f1fbc75aa1ac053890cee4efe8d5bd36e81d
Yash Chaturvedi <108249500+YashchaturvediIR@users.noreply.github.com>
2025-06-22 15:05:48 +0530

    Add .next/static/chunks/src_app_page_tsx_ab334ed5._.js 1750584941079

---
+++ .next/static/chunks/src_app_page_tsx_ab334ed5._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_090f1564._.js"
+],
+    source: "dynamic"
+});
